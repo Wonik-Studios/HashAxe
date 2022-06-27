15b250133f849c031ae551a41843c81086cdb8c2
rts4 <jgallinger4@gmail.com>
2022-06-27 13:30:52 -0400

    begin traverser

--- Traverser.cs
+++ Traverser.cs
@@ -1,4 +1,26 @@
+using HashAxe.MD5HashSet;
+
 namespace HashAxe.Traverser
 {
-    
+    public class Traverser {
+        private string path { get; set; }
+        private MD5Hash hashSet;
+
+        public Traverser(string path, MD5Hash hashSet)
+        {
+            if(!(Directory.Exists(path) || File.Exists(path))) {
+                throw new FileNotFoundException();
+            }
+            this.path = path;
+            this.hashSet = hashSet;
+        }
+
+        public void Traverse() {
+            this.Traverse(path);
+        }
+
+        private void Traverse(string dir) {
+            
+        }
+    }
 }
