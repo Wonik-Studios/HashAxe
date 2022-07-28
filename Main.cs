ac5c053b6acd8bfc1a771c8f14eebb0eed81f0ec
petal-person <aryteary@gmail.com>
2022-07-28 07:27:52 -0400

    root
    Co-authored-by: MobBarley2021 <MobBarley2021@users.noreply.github.com>

--- Main.cs
+++ Main.cs
@@ -7,49 +7,65 @@ namespace HashAxe
 {
     class HashAxeMain
     {
-		static async Task Main(string[] args)
-		{
-			Command getHashes = new Command("get-hashes", "Download the latest set of hashes.");
-			Command checkSum = new Command("checksum", "Verifies the local hashes that no alertations have occured.");
-			Command scan = new Command("scan", "Runs the HashAxe scan on a path.");
-			RootCommand rCommand = new RootCommand("This is the root command for HashAxe made by Wonik. If you are seeing this, that means HashAxe is installed properly.");
-
-			rCommand.Add(getHashes);
-			rCommand.Add(checkSum);
-			rCommand.Add(scan);
-
-			getHashes.SetHandler(async () =>
-			{
-				await Cmd_GetHashes();
-			});
-
-			checkSum.SetHandler(async () =>
-			{
-				await Cmd_Checksum();
-			});
-
-			scan.SetHandler(async () =>
-			{
-				await Cmd_Scan();
-			});
-
-			await rCommand.InvokeAsync(args);
-		}
-        internal static async Task Cmd_GetHashes()
+        static async Task Main(string[] args)
         {
-			Console.WriteLine("This command is being written!");
+            Command getHashes = new Command("get-hashes", "Download the latest set of hashes.");
+            Command checkSum = new Command("checksum", "Verifies the local hashes that no alertations have occured.");
+            Command scan = new Command("scan", "Runs the HashAxe scan on a path.");
+            RootCommand rCommand = new RootCommand("This is the root command for HashAxe made by Wonik. If you are seeing this, that means HashAxe is installed properly.");
+
+            rCommand.Add(getHashes);
+            rCommand.Add(checkSum);
+            rCommand.Add(scan);
+
+            getHashes.SetHandler(async () =>
+            {
+                string launchPath = root();
+                await Cmd_GetHashes(launchPath);
+            });
+
+            checkSum.SetHandler(async () =>
+            {
+                string launchPath = root();
+                await Cmd_Checksum(launchPath);
+            });
+
+            scan.SetHandler(async () =>
+            {
+                string launchPath = root();
+                await Cmd_Scan(launchPath);
+            });
+
+            await rCommand.InvokeAsync(args);
+        }
+
+        internal static string root()
+        {
+            Console.WriteLine(@"
+██╗░░██╗░█████╗░░██████╗██╗░░██╗░█████╗░██╗░░██╗███████╗
+██║░░██║██╔══██╗██╔════╝██║░░██║██╔══██╗╚██╗██╔╝██╔════╝
+███████║███████║╚█████╗░███████║███████║░╚███╔╝░█████╗░░
+██╔══██║██╔══██║░╚═══██╗██╔══██║██╔══██║░██╔██╗░██╔══╝░░
+██║░░██║██║░░██║██████╔╝██║░░██║██║░░██║██╔╝╚██╗███████╗
+╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚══════╝");
+			Console.WriteLine("HasAxe {version} by Wonik");
+            return "I like to move it move it";
+        }
+        internal static async Task Cmd_GetHashes(string hashaxe_root)
+        {
+            Console.WriteLine(hashaxe_root);
             return;
         }
 
-		internal static async Task Cmd_Checksum()
+        internal static async Task Cmd_Checksum(string hashaxe_root)
         {
-			Console.WriteLine("This command is being written!");
+            Console.WriteLine(hashaxe_root);
             return;
         }
 
-		internal static async Task Cmd_Scan()
+        internal static async Task Cmd_Scan(string hashaxe_root)
         {
-			Console.WriteLine("This command is being written!");
+            Console.WriteLine(hashaxe_root);
             return;
         }
     }
